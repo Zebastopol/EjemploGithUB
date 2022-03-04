8ac7f798e9f2d83fa8add1591c07537e635ff1ac
Helton Bustos <heltonsmith@hotmail.com>
2022-03-04 20:02:31 -0300

    hb: He cambiado el texto y he agregado un boton de saludo con un Toast

--- app/src/main/java/com/sebastopol/ejemplogithub/MainActivity.java
+++ app/src/main/java/com/sebastopol/ejemplogithub/MainActivity.java
@@ -3,12 +3,26 @@ package com.sebastopol.ejemplogithub;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnSaludar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSaludar = findViewById(R.id.btnSaludar);
+
+        btnSaludar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "HOLA A TODOS LOS DE GITHUB", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
